db0632bfd8d094422934520055288f72621cae6f
pascalDecor <77197221+pascalDecor@users.noreply.github.com>
2025-07-15 11:00:02 +0000

    update de /uplaod

--- src/app/api/upload/route.tsx
+++ src/app/api/upload/route.tsx
@@ -6,23 +6,30 @@ const supabase = createClient(
   process.env.SUPABASE_SERVICE_ROLE_KEY! // ou SUPABASE_ANON_KEY si le bucket est public
 );
 
+const sanitizeFileName = (name: string) =>
+  name
+    .normalize('NFD')
+    .replace(/[\u0300-\u036f]/g, '')
+    .replace(/[^a-zA-Z0-9.\-_]/g, '-')
+    .toLowerCase();
+
 export async function POST(req: Request) {
   const formData = await req.formData();
   const file = formData.get('image') as File;
-  
+
 
   if (!file) {
     return NextResponse.json({ error: 'No file provided' }, { status: 400 });
   }
 
-  const filename = `${Date.now()}-${file.name.replace(/\s/g, '-')}`;
-
-  const path = `public/${filename}`;
+  const originalName = file.name;
+  const safeName = sanitizeFileName(originalName);
+  const path = `public/${Date.now()}-${safeName}`;
 
   // Upload vers Supabase Storage
   const { data, error } = await supabase.storage
     .from(process.env.SUPABASE_BUCKET_NAME!) // Remplace par le nom de ton bucket
-    .upload(path , file, {
+    .upload(path, file, {
       contentType: file.type,
       cacheControl: '3600',
       upsert: false,
@@ -34,7 +41,7 @@ export async function POST(req: Request) {
 
   const { data: publicUrlData } = supabase.storage
     .from('my-bucket')
-    .getPublicUrl(`uploads/${filename}`);
+    .getPublicUrl(`uploads/${safeName}`);
 
   return NextResponse.json({
     success: 1,
