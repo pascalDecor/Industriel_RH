5a2f6d0134afac87c58ee419b45cbdea9d14138f
pascalDecor <77197221+pascalDecor@users.noreply.github.com>
2025-08-14 15:09:48 +0000

    Mise      en place  de la                gestion des articles

--- src/constant/api.tsx
+++ src/constant/api.tsx
@@ -7,13 +7,13 @@ const PROD_API_BASE_URL = "https://industriel-rh.vercel.app/api"
 const PROD_API_BASE = "https://api.industriel-rh.com"
 const PROD_API_BASE_WITHOUT_PROTOCOL = "api.industriel-rh.com"
 
-const DEV_API_BASE_URL = "http://localhost:3000/api" 
+const DEV_API_BASE_URL = "http://localhost:3000/api"
 const DEV_API_BASE = "http://localhost:3000/api"
 const DEV_API_BASE_WITHOUT_PROTOCOL = "localhost:3000/api";
 
 export const apiBaseURL = isProd ? PROD_API_BASE_URL : DEV_API_BASE_URL;
 
-export const baseApiURL: string = "http://localhost:3000/api";
+export const baseApiURL: string = isProd ? PROD_API_BASE_URL : DEV_API_BASE_URL;
 export const apiBase: string = isProd ? PROD_API_BASE : DEV_API_BASE;
 export const apiBaseWithoutProtocol: string = isProd ? PROD_API_BASE_WITHOUT_PROTOCOL : DEV_API_BASE_WITHOUT_PROTOCOL;
 
