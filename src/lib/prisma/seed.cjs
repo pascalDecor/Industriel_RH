dd60dbd9c3feae202ebd571f3c0a0322590363dc
pascalDecor <77197221+pascalDecor@users.noreply.github.com>
2026-02-13 09:53:20 +0000

    update

---
+++ src/lib/prisma/seed.cjs
@@ -0,0 +1,32 @@
+import { PrismaClient } from '@prisma/client';
+
+import createSectors from './seeders/createSectors';
+import createSpecialites from './seeders/createSpecialites';
+import createCivilities from './seeders/createCivilities';
+import createCities from './seeders/createCities';
+import createCountries from './seeders/createCountries';
+import createTags from './seeders/createTags';
+import createNotices from './seeders/createNotices';
+import createUsers from './seeders/createUsers';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  await createSpecialites(prisma);
+  await createSectors(prisma);
+  await createCivilities(prisma);
+  await createCountries(prisma);
+  await createCities(prisma);
+  await createTags(prisma);
+  await createNotices(prisma);
+  await createUsers(prisma);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
